6867a69eb01a9c5af0c7e22b13622d9ba78b3daa
Hiroaki Miura <hiroaki@oisu.jp>
2025-02-16 03:01:47 +0900

    feat: do something

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,10 +1,10 @@
 import { Redirect, Stack } from "expo-router";
-import { useContext } from "react";
+import React, { useContext } from "react";
+import CameraButton from "~/components/camera-button";
 import { AuthContext } from "~/contexts/auth-context";
 
 export default function AppLayout() {
   const { session } = useContext(AuthContext);
-
   if (session === undefined) {
     return null;
   }
@@ -13,8 +13,19 @@ export default function AppLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <React.Fragment>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="camera"
+          options={{
+            headerShown: false,
+            presentation: "modal",
+            title: "カメラ",
+          }}
+        />
+      </Stack>
+      <CameraButton />
+    </React.Fragment>
   );
 }
---
+++ components/camera-button.tsx
@@ -0,0 +1,29 @@
+import { Link } from "expo-router";
+import { Camera } from "lucide-react-native";
+import React from "react";
+import { useWindowDimensions } from "react-native";
+import { Button } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+
+export default function CameraButton() {
+  const { width } = useWindowDimensions();
+  const buttonSize = 60;
+  return (
+    <Link asChild href="camera">
+      <Button
+        className={cn(
+          "absolute z-20 rounded-full bg-white text-primary border-primary border-2",
+          `left-[${width / 2 - 20}px] bottom-20`
+        )}
+        style={{
+          left: width / 2 - buttonSize / 2,
+          bottom: buttonSize,
+          width: buttonSize,
+          height: buttonSize,
+        }}
+      >
+        <Camera className="size-10" />
+      </Button>
+    </Link>
+  );
+}
